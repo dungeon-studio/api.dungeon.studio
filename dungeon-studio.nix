9e3d147fd45f12bdd33177373e459bce9e2ea4fd
Alex Brandt <alunduil@alunduil.com>
2017-09-29 18:59:26 -0500

    update dependencies
    I've upgraded to NixOS 17.09 and have access to different Haskell
    versions.  I'm changing the dependencies accordingly but feel free to
    keep old or newer versions working as necessary.

--- dungeon-studio.nix
+++ dungeon-studio.nix
@@ -1,9 +1,8 @@
 { mkDerivation, aeson, base, bytestring, containers, data-default
-, directory, either, envy, exceptions, extra, filepath, hasbolt
+, directory, envy, exceptions, extra, filepath, hasbolt
 , http-api-data, http-media, http-types, MissingH, mtl, network-uri
-, QuickCheck, quickcheck-instances, resource-pool, retry, servant
-, servant-server, stdenv, test-invariant, text, time
-, unordered-containers, uuid, vector, wai-logger, warp, yaml
+, resource-pool, retry, servant, servant-server, stdenv, text, time
+, unordered-containers, uuid, wai-logger, warp, yaml
 }:
 mkDerivation {
   pname = "dungeon-studio";
@@ -12,16 +11,11 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    aeson base bytestring containers data-default directory either envy
+    aeson base bytestring containers data-default directory envy
     exceptions extra filepath hasbolt http-api-data http-media
     http-types MissingH mtl network-uri resource-pool retry servant
-    servant-server text time unordered-containers uuid vector
-    wai-logger warp yaml
-  ];
-  testHaskellDepends = [
-    aeson base bytestring containers http-api-data http-media
-    http-types MissingH network-uri QuickCheck quickcheck-instances
-    test-invariant text unordered-containers
+    servant-server text time unordered-containers uuid wai-logger warp
+    yaml
   ];
   homepage = "https://github.com/alunduil/dungeon.studio";
   description = "Game Master's Companion";

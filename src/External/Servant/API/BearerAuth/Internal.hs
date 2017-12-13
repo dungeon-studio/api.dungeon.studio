ead53f6d498143c6e1fe17fbcb2fa4b3a42a7080
Alex Brandt <alunduil@alunduil.com>
2017-12-13 09:58:14 -0600

    add audience values to 401 and 403 errors
    Fixes #54

--- src/External/Servant/API/BearerAuth/Internal.hs
+++ src/External/Servant/API/BearerAuth/Internal.hs
@@ -3,77 +3,27 @@
 
 {-|
 Module      : External.Servant.API.BearerAuth.Internal
-Description : Servant Bearer Authentication Internals
+Description : Servant Bearer Authorization
 Copyright   : (c) Alex Brandt, 2017
 License     : MIT
 
-"Servant" types for Bearer authentication.
+"Servant" types for Bearer authorization.
 -}
 module External.Servant.API.BearerAuth.Internal where
 
 import Control.Lens ((^?), (^.))
-import Control.Monad.Catch (Exception, MonadThrow, throwM)
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad ((<=<))
-import Crypto.JOSE (JWKSet)
+import Control.Monad.Catch (MonadThrow, throwM)
 import Crypto.JWT (ClaimsSet, claimSub, string, unregisteredClaims)
-import Data.Aeson (eitherDecode, fromJSON, Result (Error, Success))
-import Data.List (isInfixOf)
-import Data.Text (Text)
-import Data.Typeable (Typeable)
-import Network.HTTP.Simple (getResponseBody, httpLBS, parseRequest)
-import Network.HTTP.Types.Header (hWWWAuthenticate)
-import Network.URI (URI, uriToString)
-import Network.Wai (Request, requestHeaders)
-import Servant (err401, ServantErr (errHeaders))
-import Text.Regex (mkRegex, subRegex)
+import Data.Aeson (fromJSON, Result (Error, Success))
+import Network.URI (URI)
 
-import qualified Data.ByteString as BS (stripPrefix)
-import qualified Data.ByteString.Char8 as BS (pack)
-import qualified Data.ByteString.Lazy as BL (ByteString, fromStrict)
-import qualified Data.ByteString.Lazy.Char8 as BL (pack, unpack)
 import qualified Data.HashMap.Strict as Map (lookup)
 import qualified Data.Text as T (pack, words)
 
-import External.Network.URI.HttpApiData ()
+import External.Servant.API.BearerAuth.Types
 
--- | Subject and Scope from JWT.
---
---   TODO Consider other claims to be passed through.
---   TODO Consider a class as well, so others may define their own.
-data Claims = Claims
-  { sub   :: Text
-  , scope :: [Text]
-  }
-  deriving (Eq, Show)
-
-data BearerError = InvalidToken String
-                 | InsufficientScope String
-                 | MissingAuthorization
-  deriving (Typeable)
-
-instance Show BearerError where
-  show (InvalidToken d)      = "invalid_token: " ++ d
-  show (InsufficientScope d) = "insufficient_scope: " ++ d
-  show MissingAuthorization  = "missing authorization"
-
-instance Exception BearerError
-
-jwks :: (MonadThrow m, MonadIO m) => URI -> m JWKSet
-jwks u =
-  do r  <- parseRequest $ uriToString id u ""
-     bs <- fix . getResponseBody <$> httpLBS r
-     either fail return $ eitherDecode bs
-  where fix = if "auth0" `isInfixOf` uriToString id u ""
-                 then BL.pack . flip (subRegex re) "" . BL.unpack -- HACK for Auth0
-                 else id
-        re  = mkRegex "[,{][[:space:]]*\"x5t\":[^,}]+"
-
-compact :: Request -> Maybe BL.ByteString
-compact = fmap BL.fromStrict . (BS.stripPrefix "Bearer " <=< lookup "authorization") . requestHeaders
-
-claims :: (MonadThrow m) => ClaimsSet -> m Claims
-claims c =
+claims :: (MonadThrow m) => URI -> ClaimsSet -> m Claims
+claims a c =
   do sub'  <- maybe (throwM $ InvalidToken "sub claim missing") return (c ^. claimSub)
      sub   <- maybe (throwM $ InvalidToken "sub claim invalid") (return . T.pack) (sub' ^? string)
 
@@ -82,12 +32,6 @@ claims c =
                 Success x -> return $ T.words x
                 Error m   -> throwM $ InvalidToken $ "scope claim invalid: " ++ m
 
+     let audience = a
+
      return Claims {..}
-
-e401 :: BearerError -> ServantErr
-e401 e = err401 { errHeaders = [ (hWWWAuthenticate, BS.pack (h e)) ] }
-  where h MissingAuthorization  = p
-        h (InvalidToken d)      = p ++ ", error=\"invalid_token\", error_description=\"" ++ d ++ "\""
-        h (InsufficientScope d) = p ++ ", error=\"insufficient_scope\", error_description=\"" ++ d ++ "\""
-
-        p = "Bearer realm=\"AUDIENCE\"" :: String -- TODO Fill in Audience
--- test/External/Servant/API/BearerAuth/InternalSpec.hs
+++ test/External/Servant/API/BearerAuth/InternalSpec.hs
@@ -13,11 +13,13 @@ module External.Servant.API.BearerAuth.InternalSpec (main, spec) where
 import Control.Lens ((.~), (&))
 import Crypto.JWT (claimSub, emptyClaimsSet, unregisteredClaims)
 import Data.Aeson (Value (String))
+import Network.URI (nullURI)
 import Test.Hspec (describe, hspec, it, pending, Selector, shouldReturn, shouldThrow, Spec)
 
 import qualified Data.HashMap.Lazy as HashMap (fromList)
 
 import External.Servant.API.BearerAuth.Internal
+import External.Servant.API.BearerAuth.Types
 
 main :: IO ()
 main = hspec spec
@@ -27,17 +29,17 @@ spec =
   describe "claims" $
     do it "should throw InvalidToken \"sub claim missing\"" $
          let cs = emptyClaimsSet
-         in claims cs `shouldThrow` invalidToken "sub claim missing"
+         in claims nullURI cs `shouldThrow` invalidToken "sub claim missing"
 
        it "should throw InvalidToken \"sub claim invalid\"" $
          let cs = emptyClaimsSet &
                     claimSub .~ Just "mailto://user@example.com"
-         in claims cs `shouldThrow` invalidToken "sub claim invalid"
+         in claims nullURI cs `shouldThrow` invalidToken "sub claim invalid"
 
        it "should throw InvalidToken \"scope claim missing\"" $
          let cs = emptyClaimsSet &
                     claimSub .~ Just "user@example.com"
-         in claims cs `shouldThrow` invalidToken "scope claim missing"
+         in claims nullURI cs `shouldThrow` invalidToken "scope claim missing"
 
        it "should throw InvalidToken \"scope claim invalid\"" pending
 
@@ -45,7 +47,7 @@ spec =
          let cs = emptyClaimsSet &
                     claimSub .~ Just "subject" &
                     unregisteredClaims .~ HashMap.fromList [ ("scope", String "s1 s2") ]
-         in claims cs `shouldReturn` Claims { sub = "subject", scope = [ "s1", "s2" ] } 
+         in claims nullURI cs `shouldReturn` Claims { sub = "subject", scope = [ "s1", "s2" ], audience = nullURI } 
 
 invalidToken :: String -> Selector BearerError
 invalidToken s (InvalidToken s') = s == s'

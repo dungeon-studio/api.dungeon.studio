71e4f35535d6e54a10a0ee6deda31fb127bb9805
Alex Brandt <alunduil@gmail.com>
2018-12-16 09:52:27 -0800

    Fix hlint warnings.

--- test/External/Servant/API/BearerAuth/InternalSpec.hs
+++ test/External/Servant/API/BearerAuth/InternalSpec.hs
@@ -33,19 +33,19 @@ spec =
 
        it "should throw InvalidToken \"sub claim invalid\"" $
          let cs = emptyClaimsSet &
-                    claimSub .~ Just "mailto://user@example.com"
+                    (claimSub ?~ Just "mailto://user@example.com")
          in claims nullURI cs `shouldThrow` invalidToken "sub claim invalid"
 
        it "should throw InvalidToken \"scope claim missing\"" $
          let cs = emptyClaimsSet &
-                    claimSub .~ Just "user@example.com"
+                    (claimSub ?~ Just "user@example.com")
          in claims nullURI cs `shouldThrow` invalidToken "scope claim missing"
 
        it "should throw InvalidToken \"scope claim invalid\"" pending
 
        it "should return valid Claims" $
          let cs = emptyClaimsSet &
-                    claimSub .~ Just "subject" &
+                    (claimSub ?~ Just "subject") &
                     unregisteredClaims .~ HashMap.fromList [ ("scope", String "s1 s2") ]
          in claims nullURI cs `shouldReturn` Claims { sub = "subject", scope = [ "s1", "s2" ], audience = nullURI } 
 
